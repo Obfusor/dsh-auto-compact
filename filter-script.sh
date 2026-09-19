8e153c7be8ebca5ac0c82cab93c0a94b969d6c8c
Obfusor <Obfusor>
2026-09-19 17:45:19 -0400

    chore: remove temporary filter script

--- filter-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ "$GIT_AUTHOR_NAME" = "Johnathon Ye" ]; then
-    export GIT_AUTHOR_NAME="Obfusor"
-    export GIT_AUTHOR_EMAIL="Obfusor"
-fi
-if [ "$GIT_COMMITTER_NAME" = "Johnathon Ye" ]; then
-    export GIT_COMMITTER_NAME="Obfusor"
-    export GIT_COMMITTER_EMAIL="Obfusor"
-fi
